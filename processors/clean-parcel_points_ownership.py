1d09806cc429b913f90e1297c054e577eb732b2f
kat-alo <katlyn@stanford.edu>
2019-08-22 16:59:16 -0400

    officially retiring main housing cleanup file, replacing with parcel points cleanup

--- processors/clean-parcel_points_ownership.py
+++ processors/clean-parcel_points_ownership.py
@@ -11,40 +11,13 @@ VALID_ZIPCODES = [
 	"48240", "48226", "48239", "48219", "48209", "48210", "48206", "48214", "48202", "48204", "48213",
 	"48238", "48203", "48211", "48208", "48212", "48236", "48225", "48205", "48227"]
 
-NEW_CSV_HEADERS = ["ADDRESS", "ZIP-CODE", "PARCEL_POINTS_TAXPAYER", "PARCEL_POINTS_OWNER", "ZIP-MODIFIED", "CLEAN-ZIP-CODE"]
+NEW_CSV_HEADERS = ["ADDRESS", "ZIP-CODE", "PARCEL_POINTS_TAXPAYER", "PARCEL_POINTS_OWNER", "PARCEL_POINTS_PRE", "ZIP-MODIFIED", "CLEAN-ZIP-CODE", "CORRECTED_PRE", "PRE_SAME"]
 
-OLD_CSV_HEADERS = ["Address", "Zip Code", "Taxpayer", "Owner"]
-
-GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+OLD_CSV_HEADERS = ["Address", "Zip Code", "Taxpayer", "Owner", "Principal Residence Exemption"]
 
 GEOCODIO_ZIPS = 'data/static/geocodio-zip-codes.csv'
 
-
-def get_zip_from_google(address):
-	#'Okay, Google. What is the zip code for this address?'
-	params = {
-		'address' :  (address + " Detroit, MI"),
-		'key' : os.getenv("GOOGLE_MAPS_API_KEY")
-	}
-
-	req = requests.get(GOOGLE_MAPS_API_URL, params=params)
-	res = req.json()
-
-	if len(res['results']) < 1:
-		return "ERROR NO RESULTS FOUND FOR THIS ADDRESS"
-
-	#assumes first result is best result
-	result = res['results'][0]
-
-	address_components = result["address_components"]
-
-	for component in address_components:
-		if component['types'] and 'postal_code' in component.get('types'):
-			new_zip = component["short_name"]
-			return new_zip
-			break
-
-	return "ERROR NO ZIP FOUND FOR ADDRESS"
+CORRECTED_PRE_FILENAME = 'data/static/corrected_assessor_parcel_received_060819.csv'
 
 #getting zip from CSV that came from geocodio processing Parcel Points Ownership, mid-March 2019
 def get_zip_from_data(address):
@@ -57,8 +30,71 @@ def get_zip_from_data(address):
 
 	return item
 
+def is_row_empty(row):
+
+	for key in row.keys():
+		if row[key].strip() != "":
+			return False
+
+	return True
+
+def add_zip_values(row, new_row):
+	#if the zip code is not one of the valid zip codes listed, we need to modify it and flag it as modified
+	if row["Zip Code"] not in VALID_ZIPCODES and row['Address'].strip() != "":
+		#indicate that the zip code was modified
+		new_row.append("true")
+
+		new_zip = get_zip_from_data(row['Address'])
+
+		new_row.append(new_zip)
+
+	elif row['Address'].strip() == "":
+		new_row.append("N/A")
+		new_row.append("N/A")
+
+	else:
+		#indicate that zip code was not modified
+		new_row.append("false")
+
+		#make new zip code same as original one
+		new_row.append(row["Zip Code"])
+
+	return new_row
+
+def add_pre_values(new_row, data):
+
+	if data.get(new_row[0].strip().upper()):
+		
+		new_row.append(data[new_row[0].strip().upper()])
+
+	else:
+		new_row.append("ADDRESS NOT FOUND")
+
+	if new_row[4].strip().upper() == new_row[7].strip().upper():
+		new_row.append("True")
+	else:
+		new_row.append("False")
+
+	return new_row
+
+def get_corrected_pre_data():
+
+	data = {}
+
+	with open(CORRECTED_PRE_FILENAME, encoding="ISO-8859-1") as f:
+
+		reader = csv.DictReader(f)
+
+		for row in reader:
+
+			data[row['PropertyAddressCombined']] = row['ParcelsMayPRE']
+
+	return data
 
 def main(raw_filename, clean_filename):
+
+	corrected_pre_data = get_corrected_pre_data()
+
 	#create a reader for the raw housing csv
 	with open(raw_filename) as fr:
 		reader = csv.DictReader(fr)

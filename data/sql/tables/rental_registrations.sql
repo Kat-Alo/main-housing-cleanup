1652283c6125fcbe96c17bf8af38e0d5a3bb79fe
kat-alo <katlyn@stanford.edu>
2019-10-21 23:36:46 -0400

    changing SQL tables based on what data is now available in the portal

--- data/sql/tables/rental_registrations.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE public.rental_registrations (
-  ADDRESS character varying,
-  RENTAL_REG_STATUS character varying
-);
--- data/sql/tables/upcoming_demolitions.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE public.upcoming_demolitions(
-	ADDRESS character varying,
-	DEMOLITION_CONTRACTOR character varying,
-	PROJECTED_DEMOLISH_BY_DATE character varying
-);
